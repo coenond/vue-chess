0f0af74e7daa497da55964e5f0d12b6dd969db00
Coen Onderwater <coenonderwater@gmail.com>
2020-12-02 22:14:16 +0100

    feature: add view for the game

--- src/views/Game.vue
+++ src/views/Game.vue
@@ -1,5 +1,18 @@
 <template>
   <div class="game">
     <h1>Chess Game</h1>
+    <board />
   </div>
 </template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import Board from '@/components/representation/Board.vue';
+
+export default defineComponent({
+  name: 'Game',
+  components: {
+    Board,
+  },
+});
+</script>
