3082269a46de26b6927e0b968d78d4a31b7e9020
Coen Onderwater <coenonderwater@gmail.com>
2020-12-02 22:13:24 +0100

    feature: add helper class to determine the color of a square

---
+++ src/helpers/SquareColorHelper.ts
@@ -0,0 +1,27 @@
+import Square from '@/models/square/Square';
+import File from '@/models/square/File';
+import Rank from '@/models/square/Rank';
+import Color from '@/models/square/Color';
+
+class SquareColorHelper {
+  static getColorForSquare(square: Square): Color {
+    return this.isBlack(square.getFile(), square.getRank())
+      ? Color.Black : Color.White;
+  }
+
+  static getColorForSignature(file: File, rank: Rank): Color {
+    return this.isBlack(file, rank)
+      ? Color.Black : Color.White;
+  }
+
+  private static isBlack(file: File, rank: Rank): boolean {
+    return (
+      ['A', 'C', 'E', 'F'].includes(file.name())
+      && ['1', '3', '6', '7'].includes(rank.name())
+    ) || (
+      ['B', 'D', 'F', 'H'].includes(file.name())
+      && ['2', '4', '6', '8'].includes(rank.name())
+    );
+  }
+}
+export default SquareColorHelper;
