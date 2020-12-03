0e172d7da448719f2f5d4baac7e9c464e4dd2ffd
Coen Onderwater <coenonderwater@gmail.com>
2020-12-03 17:48:55 +0100

    feature: draw the board on the page using css-grid

---
+++ src/components/representation/Square.vue
@@ -0,0 +1,33 @@
+<template>
+  <div
+    class="square"
+    :class="colorClass">
+    <p>{{ squareObj.name() }}</p>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import SquareColorHelper from '@/helpers/SquareColorHelper';
+import Color from '@/models/square/Color';
+import Square from '@/models/square/Square';
+
+export default defineComponent({
+  name: 'Square',
+  props: {
+    squareObj: {
+      type: Square,
+      required: true,
+    },
+  },
+  computed: {
+    color(): Color {
+      return SquareColorHelper.getColorForSquare(this.squareObj);
+    },
+    colorClass(): string {
+      console.log(`square__${this.color}`);
+      return `square__${this.color}`;
+    },
+  },
+});
+</script>
