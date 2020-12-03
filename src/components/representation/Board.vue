722151e661e3c5e3dfebb11a78cea761c08bb72e
Coen Onderwater <coenonderwater@gmail.com>
2020-12-03 18:01:07 +0100

    chore: remove unused data from Board view

--- src/components/representation/Board.vue
+++ src/components/representation/Board.vue
@@ -9,20 +9,12 @@
 <script lang="ts">
 import { defineComponent } from 'vue';
 import SquareObj from '@/models/square/Square';
-import File from '@/models/square/File';
-import Rank from '@/models/square/Rank';
 import Square from '@/components/representation/Square.vue';
 
 export default defineComponent({
   name: 'Board',
   components: {
     Square,
-  },
-  data() {
-    return {
-      files: File.all(),
-      ranks: Rank.all(),
-    };
   },
   computed: {
     allSquares(): SquareObj[] {
