f48dad0a2def7a2c9592b3c0f6283dce3c88d5fe
Coen Onderwater <coenonderwater@gmail.com>
2020-12-13 22:42:39 +0100

    feature: add movement generation for Bishop

--- src/models/pieces/Bishop.ts
+++ src/models/pieces/Bishop.ts
@@ -2,12 +2,20 @@ import Piece from '@/models/pieces/Piece';
 import ColorEnum from '@/models/common/ColorEnum';
 
 class Bishop extends Piece {
-  private static readonly pieceName: string = 'Bishop';
+  private static readonly _pieceName: string = 'Bishop';
 
-  private static readonly pieceNotation: string = 'b';
+  private static readonly _pieceNotation: string = 'b';
 
   constructor(color: ColorEnum) {
     super(color, Bishop.pieceName, Bishop.pieceNotation);
+  }
+
+  static get pieceName(): string {
+    return this._pieceName;
+  }
+
+  static get pieceNotation(): string {
+    return this._pieceNotation;
   }
 }
 export default Bishop;
