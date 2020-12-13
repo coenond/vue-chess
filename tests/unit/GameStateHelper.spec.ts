eb3e3349162e72f0658087b7efe795992eb26c27
Coen <coen@oaky.com>
2020-12-13 17:31:08 +0100

    feature: add filter for onboard and offboard indexes

--- tests/unit/GameStateHelper.spec.ts
+++ tests/unit/GameStateHelper.spec.ts
@@ -1,11 +1,18 @@
 import GameStateHelper from '@/helpers/GameStateHelper';
+import State from '@/models/game/State';
 import onBoardIndexes from './stub/OnBoardIndexes';
 
 describe('GameStateHelper.ts', () => {
+
   it('can determine all indexes on chess Board', () => {
     onBoardIndexes.forEach((index: number) => {
       expect(GameStateHelper.indexIsOnBoard(index))
         .toBeTruthy;
     });
+  });
+
+  it('can filter on board indexes', () => {
+    const allIndexes: number[] = State.allIndexes;
+    expect(GameStateHelper.filterOnBoardIndexes(allIndexes).length).toBe(64);
   });
 });
