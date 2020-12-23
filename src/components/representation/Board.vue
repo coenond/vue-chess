75c1ce33b144e94a15e6bc53a0151bd04d779b75
Coen Onderwater <coenonderwater@gmail.com>
2020-12-23 22:20:33 +0100

    feature: color movement one-by-one

--- src/components/representation/Board.vue
+++ src/components/representation/Board.vue
@@ -1,4 +1,5 @@
 <template>
+  <p>Next to move: <b>{{ nextToMove }}</b></p>
   <div class="board">
     <square v-for='s in allSquares'
       :key='s.name'
@@ -19,6 +20,7 @@ import SquareObj from '@/models/square/Square';
 import State from '@/models/game/State';
 import Square from '@/components/representation/Square.vue';
 import StateBoardHelper from '@/helpers/StateBoardHelper';
+import ColorEnum from '@/models/common/ColorEnum';
 
 export default defineComponent({
   name: 'Board',
@@ -37,6 +39,8 @@ export default defineComponent({
       squareSelected: null as SquareObj | null,
       pieceSelected: null as PieceObj | null,
       latestState: this.state,
+      // TODO: Refactor 'lastMove' when game-move-history is implemented
+      lastMovedColor: ColorEnum.Black,
     }
   },
   watch: {
@@ -48,12 +52,16 @@ export default defineComponent({
     allSquares(): SquareObj[] {
       return SquareObj.all();
     },
+    nextToMove(): ColorEnum {
+      return this.lastMovedColor === ColorEnum.Black ? ColorEnum.White : ColorEnum.Black;
+    }
   },
   methods: {
     squareClicked(clickedSquare: SquareObj) {
       // Move piece if highlighted square is clicked
       if (this.highlights.includes(clickedSquare.name) && !!this.pieceSelected && !!this.squareSelected) {
         this.latestState = this.latestState.movePiece(this.pieceSelected, this.squareSelected, clickedSquare);
+        this.lastMovedColor = this.pieceSelected.color;
         this.resetSelected();
         return;
       }
@@ -65,6 +73,11 @@ export default defineComponent({
       return this.state.pieceOnSquare(square);
     },
     setSelected(piece: PieceObj, square: SquareObj) {
+      if (piece.color === this.lastMovedColor) {
+        this.resetSelected();
+        return;
+      }
+
       this.pieceSelected = piece;
       this.squareSelected = square;
       const generator: MoveGenerator = new MoveGenerator(piece, square, this.latestState);
