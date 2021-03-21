9f50adebbe433c48785a6d8f2c9ac678fe95c9e2
Coen <coen@oaky.com>
2021-03-21 21:26:28 +0100

    chore: remove generated vue pages

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,3 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link> |
-    <router-link to="/game">game</router-link>
-  </div>
   <router-view/>
 </template>
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,22 +1,10 @@
 import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router';
-import Home from '../views/Home.vue';
-import About from '../views/About.vue';
 import Game from '../views/Game.vue';
 
 const routes: Array<RouteRecordRaw> = [
   {
     path: '/',
     name: 'Home',
-    component: Home,
-  },
-  {
-    path: '/about',
-    name: 'About',
-    component: About,
-  },
-  {
-    path: '/game',
-    name: 'Game',
     component: Game,
   },
 ];
