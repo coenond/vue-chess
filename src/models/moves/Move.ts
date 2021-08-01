e5b997a73ace99bffab84bc2a0ff4c91908421da
Coen <coenonderwater@gmail.com>
2021-08-01 22:34:24 +0200

    feature: simple but incomplete move notation

--- src/models/moves/Move.ts
+++ src/models/moves/Move.ts
@@ -2,7 +2,7 @@ import ColorEnum from "../common/ColorEnum";
 import Piece from "../pieces/Piece";
 import Square from "../square/Square";
 
-export class Move {
+export default class Move {
 
   constructor(
     public readonly moveNumber: number,
@@ -12,6 +12,22 @@ export class Move {
     public readonly piece: Piece,
     public readonly isCapture = false
   ) {
+    this.moveNumber = moveNumber;
+    this.color = color;
     this.originPosition = originPosition;
+    this.destinationPosition = destinationPosition;
+    this.piece = piece;
+    this.isCapture = isCapture;
+  }
+
+  /**
+   * @todo add Castling notation
+   * @todo add Check notation
+   * @todo add Checkmate notation
+   * @todo add which rook/knight/queen if necessary
+   */
+  get notation(): string {
+    const captureNotation = this.isCapture ? 'x' : '';
+    return `${this.piece.notation}${captureNotation}${this.destinationPosition.name}`;
   }
 }
